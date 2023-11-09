0004bfdf38d2acd55b3346d74f60d45da9f5a189
SuchDominion <shijiand2022@gmail.com>
2023-11-08 23:37:16 -0600

    Add test set to rnn

--- rnn.py
+++ rnn.py
@@ -55,19 +55,25 @@ class RNN(nn.Module):
         return predicted_vector
 
 
-def load_data(train_data, val_data):
+def load_data(train_data, val_data, test_data):
     with open(train_data) as training_f:
         training = json.load(training_f)
     with open(val_data) as valid_f:
         validation = json.load(valid_f)
+    with open(test_data) as test_f:
+        test = json.load(test_f)
 
     tra = []
     val = []
+    tes = []
     for elt in training:
         tra.append((elt["text"].split(),int(elt["stars"]-1)))
     for elt in validation:
         val.append((elt["text"].split(),int(elt["stars"]-1)))
-    return tra, val
+    for elt in test:
+        tes.append((elt["text"].split(),int(elt["stars"]-1)))
+
+    return tra, val, tes
 
 
 if __name__ == "__main__":
@@ -76,14 +82,14 @@ if __name__ == "__main__":
     parser.add_argument("-e", "--epochs", type=int, default=10, help = "num of epochs to train")
     parser.add_argument("--train_data", default="training.json", help = "path to training data")
     parser.add_argument("--val_data", default="validation.json", help = "path to validation data")
-    parser.add_argument("--test_data", default = "to fill", help = "path to test data")
+    parser.add_argument("--test_data", default = "test.json", help = "path to test data")
     parser.add_argument("--num_layers", type=int, default=1, help = "number of layers in the RNN")
     parser.add_argument("--init", type=str, default="default", choices=["default", "xavier", "kaiming"], help = "Method to initialize weights")
     parser.add_argument('--do_train', action='store_true')
     args = parser.parse_args()
 
     print("========== Loading data ==========")
-    train_data, valid_data = load_data(args.train_data, args.val_data) # X_data is a list of pairs (document, y); y in {0,1,2,3,4}
+    train_data, valid_data, test_data = load_data(args.train_data, args.val_data, args.test_data) # X_data is a list of pairs (document, y); y in {0,1,2,3,4}
 
     # Think about the type of function that an RNN describes. To apply it, you will need to convert the text data into vector representations.
     # Further, think about where the vectors will come from. There are 3 reasonable choices:
@@ -181,6 +187,29 @@ if __name__ == "__main__":
         print("Validation completed for epoch {}".format(epoch + 1))
         print("Validation accuracy for epoch {}: {}".format(epoch + 1, correct / total))
         validation_accuracy = correct/total
+
+        model.eval()
+        correct = 0
+        total = 0
+        random.shuffle(test_data)
+        print("Test started for epoch {}".format(epoch + 1))
+        test_data = test_data
+
+        for input_words, gold_label in tqdm(test_data):
+            input_words = " ".join(input_words)
+            input_words = input_words.translate(input_words.maketrans("", "", string.punctuation)).split()
+            vectors = [word_embedding[i.lower()] if i.lower() in word_embedding.keys() else word_embedding['unk'] for i
+                       in input_words]
+
+            vectors = torch.tensor(vectors).view(len(vectors), 1, -1)
+            output = model(vectors)
+            predicted_label = torch.argmax(output)
+            correct += int(predicted_label == gold_label)
+            total += 1
+            # print(predicted_label, gold_label)
+        print("Test completed for epoch {}".format(epoch + 1))
+        print("Test accuracy for epoch {}: {}".format(epoch + 1, correct / total))
+        Test_accuracy = correct/total
 
         if validation_accuracy < last_validation_accuracy and trainning_accuracy > last_train_accuracy:
             stopping_condition=True
